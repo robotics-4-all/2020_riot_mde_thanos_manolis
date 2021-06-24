21f2741498fd96d5660a1407450c50883e4e9932
Thanos Manolis <thanos.m14@gmail.com>
2021-06-24 15:12:17 +0300

    Change file structure again

--- parser.py
+++ parser.py
@@ -28,13 +28,9 @@ def main():
     # Load C template
     template1 = env.get_template(
         'templates/node.c.tmpl')
-    
-    # Load C header template
-    template2 = env.get_template(
-        'templates/mqtt_funcs.h.tmpl')
 
     # Load Makefile template
-    template3 = env.get_template(
+    template2 = env.get_template(
         'templates/Makefile.tmpl')
 
     """ Parse info from device meta-model """
@@ -84,32 +80,32 @@ def main():
 
     """ Produce source code from templates """
 
+    address_tmp = connection_model.connections[0].com_endpoint.addr
+    id_tmp = 1
+    num_of_msgs_tmp = 10
     mqtt_port = connection_model.connections[0].com_endpoint.port
     echo_pin_tmp = (connection_model.connections[0].hw_conns[0].board_int).split("_",1)[1]
     trigger_pin_tmp = (connection_model.connections[0].hw_conns[1].board_int).split("_",1)[1]
     system_name = connection_model.connections[0].name
     peripheral_name_tmp = connection_model.connections[0].peripheral.device
-    module_tmp = connection_model.includes[0].val
+
 
     # C template
-    rt = template1.render(port=mqtt_port,
-                         trigger_port=1,
-                         trigger_pin=trigger_pin_tmp,
-                         echo_port=1,
-                         echo_pin=echo_pin_tmp,
-                         perihperal_name=peripheral_name_tmp)
-    ofh = codecs.open("codegen/src/output_node.c", "w", encoding="utf-8")
-    ofh.write(rt)
-    ofh.close()
-
-    # C header template
-    rt = template2.render(port=mqtt_port)
-    ofh = codecs.open("codegen/inc/mqtt_funcs.h", "w", encoding="utf-8")
+    rt = template1.render(address=address_tmp,
+                          id=id_tmp,
+                          num_of_msgs=num_of_msgs_tmp,
+                          port=mqtt_port,
+                          trigger_port=1,
+                          trigger_pin=trigger_pin_tmp,
+                          echo_port=1,
+                          echo_pin=echo_pin_tmp,
+                          peripheral_name=peripheral_name_tmp)
+    ofh = codecs.open("codegen/output_node.c", "w", encoding="utf-8")
     ofh.write(rt)
     ofh.close()
 
     # Makefile template
-    rt = template3.render(app_name=system_name,
+    rt = template2.render(app_name=system_name,
                           module=peripheral_name_tmp)
     ofh = codecs.open("codegen/Makefile", "w", encoding="utf-8")
     ofh.write(rt)
